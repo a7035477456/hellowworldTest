39b32571535cd439316edf8c00b903915b3c2b8c
Andrew Andrew <a@b.com>
2026-02-25 13:06:56 -0500

    fix verified seal

--- fe/src/store/constant.js
+++ fe/src/store/constant.js
@@ -1,6 +1,6 @@
 // theme constant
 export const gridSpacing = 3;
-export const drawerWidth = 210;
+export const drawerWidth = 192;
 export const drawerWidthClosed = 62;
-export const drawerWidthMobile = 210;
+export const drawerWidthMobile = 192;
 export const appDrawerWidth = 320;
